8addd6c9df3811eb59dbecc118b3096025ae8c57
timshaya <17090087+timshaya@users.noreply.github.com>
2025-03-13 10:36:28 -0500

    minor error clean up

--- src/test/kotlin/structural/decorator/ViperDecoratorTest.kt
+++ src/test/kotlin/structural/decorator/ViperDecoratorTest.kt
@@ -1,7 +1,7 @@
 package structural.decorator
 
 import org.junit.jupiter.api.Test
-import org.junit.jupiter.api.Assertions.*
+import org.junit.jupiter.api.Assertions.assertEquals
 
 class ViperDecoratorTest {
 
